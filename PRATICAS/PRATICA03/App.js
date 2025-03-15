74683e265115ceceb9f0bcbcfe2ca8519e0fff64
Ingrid Maria <ingridmr857@gmail.com>
2025-03-14 21:00:54 -0300

    Realização da PRATICA03, utilização de View.

---
+++ PRATICAS/PRATICA03/App.js
@@ -0,0 +1,13 @@
+import ListeScreen from "./screens/ListScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App(){
+  return <ListeScreen />;
+  return <GalleryScreen/>;
+  return <HomeScreen />;
+  return <ProfileScreen />;
+}
+
+export default App;
