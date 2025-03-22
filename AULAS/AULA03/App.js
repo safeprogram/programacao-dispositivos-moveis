04be14a654ec4a41e6e30089c36b3c36b17ac614
Ingrid Maria <ingridmr857@gmail.com>
2025-03-21 21:53:12 -0300

    Subindo arquivo de ConversaScreen com atualizações nas pastas existentes

--- AULAS/AULA03/App.js
+++ AULAS/AULA03/App.js
@@ -1,10 +1,11 @@
+import ConversaScreen from "./screens/ConversaScreen";
 import GaleriaScreen from "./screens/GaleriaScreen";
 import ProdutoScreen from "./screens/ProdutoScreen";
 import RegistrarScreen from "./screens/RegistrarScreen";
 
 
 function App() {
-  return <ProdutoScreen />;
+  return <ConversaScreen />;
 }
 
 export default App;
