7527556b3c0b8b8a378800c14f70da80ba07c79c
Ingrid Maria <ingridmr857@gmail.com>
2025-05-23 21:11:09 -0300

    Subindo Aula 08. Interface aplicativo de pedido de comida

---
+++ AULAS/AULA06/routes/NavegacaoPilha.js
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import Perfil from '../screens/Perfil';
+
+
+const Stack = createNativeStackNavigator();
+
+function NavegacaoPilha(){
+    return(
+        <Stack.Navigator initialRouteName="Perfil">
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Perfil" component={Perfil}/>
+        </Stack.Navigator>
+    )
+}
+
+export default NavegacaoPilha;
---
+++ AULAS/AULA08/App.js
@@ -0,0 +1,21 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { LojaProvider } from "./contexts/LojaContext";
+import MainNavigator from "./routes/MainNavigator";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+
+      
+      <LojaProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </LojaProvider>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
