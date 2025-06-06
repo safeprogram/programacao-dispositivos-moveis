e7101c945428d98a4582091a34df0ed86fa7b6e1
Ingrid Maria <ingridmr857@gmail.com>
2025-06-06 20:50:02 -0300

    Penultima atividade Pratica de programação de dispositivos Moveis :)

---
+++ PRATICAS/pratica11/App.js
@@ -0,0 +1,13 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TaskProvider } from "./contexts/TaskContext";
+import TaskScreen from "./screens/TaskScreen";
+
+function App() {
+  return <SafeAreaProvider>
+    <TaskProvider>
+      <TaskScreen></TaskScreen>
+    </TaskProvider>
+    </SafeAreaProvider>;
+}
+
+export default App;
